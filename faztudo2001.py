65b7f89badf6be0c9986bb718c99e4ea03d2ee61
Gustavo dos Santos Moura <49046442+GustavoMoura42@users.noreply.github.com>
2020-05-22 12:26:57 -0300

    Update faztudo2001.py

--- faztudo2001.py
+++ faztudo2001.py
@@ -1,5 +1,4 @@
 import discord
-import os
 from random import randint
 from ast import literal_eval
 
@@ -12,9 +11,9 @@ async def RegistraMagia(message):
         variavel=literal_eval(le.read())
     msgr=message.content.lower().replace("!r","").split()
     #adiciona valor na variavel correspondente ao txt
-    #msg[0]= nome da magia;
-    #msg[-2]= dado da magia;
-    #msg[-1]= quantidade de dados
+    #msgr[0]= nome da magia;
+    #msgr[-2]= dado da magia;
+    #msgr[-1]= quantidade de dados
     variavel[" ".join(msgr[0:-2])]=f'{msgr[-2]} {msgr[-1]}'
     #escreve o valor da variavel no txt
     with open ('testetxt', 'w') as escreve:
@@ -27,19 +26,25 @@ async def CastMagia(message):
         variavel=literal_eval(le.read())
 
     #recebe os valores da chave em '[]'
-    variavel=variavel[message.content.lower().replace("!cast","").strip()]
+    valores=variavel[message.content.lower().replace("!cast","").strip()]
 
-    variavel=variavel.split()
-    await RollDice(message,variavel,True)
+    valores=valores.split()
+    await RollDice(message,valores,True)
     
 async def CharlieBrown(message):
     await message.channel.send('brown'+message.content.lower().replace("charlie","").replace("!",""))
 
-async def RollDice(x, y, message):
-    # x = tipo de dado
-    # y = vezes a rolar
-    saida = [randint(1, x) for i in range(y)]
-    await message.channel.send(saida)
+async def RollDice(message=None, valores=None, flag=False):
+    #lista[0]=tipo de dado
+    #lista[1]=vezes a rolar
+    resul=[]
+    if flag:
+        lista=valores
+    else:
+        lista=message.content.lower().replace("!d","").split()
+    for x in range(int(lista[1])):
+        resul.append(randint(1,int(lista[0].replace("d",""))))
+    await message.channel.send(str(resul)[1:-1])
         
     
 @client.event
@@ -49,23 +54,16 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user: return None
-    tmp = str(message.content.lower())
-    if "d" in tmp:
-         qtd = str(tmp[0:tmp.find("d")])
-         lados = str(tmp[tmp.find("d")+1:])
-         if qtd.isdigit() and lados.isdigit():
-              await RollDice(lados, qtd, message)
+        
     if message.content.lower().startswith('!'):
         if message.content.lower().replace("!","").startswith('charlie'):
             await CharlieBrown(message)
-
-        
-
-            
+        if message.content.lower().replace("!","").startswith('d'):
+            await RollDice(message)
         if message.content.lower().replace("!","").startswith('r'):
             await RegistraMagia(message)
         if message.content.lower().replace("!","").startswith('cast'):
             await CastMagia(message)
         
 
-client.run(os.environ['mudaessafitan'])
+client.run('your token here')
